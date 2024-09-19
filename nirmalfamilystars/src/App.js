11f38479c368b20b4562b9b1a4b68b029b75d422
7schinraj <7schinraj@gmail.com>
2024-09-19 17:14:15 +0530

    alone table

--- nirmalfamilystars/src/App.js
+++ nirmalfamilystars/src/App.js
@@ -3,11 +3,12 @@ import './App.css';
 import Nav from './Components/Navbar/Nav';
 import Dashboard from './Components/Dashboard/Dashboard.jsx';
 import Login from './Components/LoginComp/Login.jsx';
+import Income from './Components/IncomeComp/Income.jsx';
 
 function App() {
   return (
     <div className="App">
-      <Login />
+      <Income />
     </div>
   );
 }
---
+++ nirmalfamilystars/src/Components/IncomeComp/Income.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import './Income.css'
+const Income = () => {
+  return (
+    <div className='income-container'>
+        <h1 className='logo'>Logo</h1>
+        <div className='main-container'>
+            <form className='form-container' action="">
+                <input type="text" placeholder='Income'/>
+                <input type="number" placeholder='Amount'/>
+                <select name="" id="">
+                    <option value="">Category-1</option>
+                    <option value="">Category-2</option>
+                    <option value="">Category-3</option>
+                    <option value="">Category-4</option>
+                    <option value="">Category-5</option>
+                </select>
+                <input type="date" />
+                
+                <textarea name="" id="" placeholder='Description'></textarea>
+                <div className='btn-container'>
+                    <button>Submit</button>
+                </div>
+            </form>
+            <table className='table-container'>
+                <tr className='rows'>
+                    <th>Income</th>
+                    <th>Amount</th>
+                    <th>Category</th>
+                    <th>Date</th>
+                    <th>Description</th>
+                    <th></th>
+                </tr>
+                <tr>
+                    <td>Income</td>
+                    <td>Amount</td>
+                    <td>Category</td>
+                    <td>Date</td>
+                    <td className='des'>Description</td>
+                    <td><button>Edit</button></td>
+                </tr>
+            </table>
+        </div>
+    </div>
+  )
+}
+
+export default Income
